c92b15177419014370ee08e828af523331c65147
Will R <williamp.robertson@gmail.com>
2019-02-04 21:56:46 -0700

    added children props to button

--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -31,7 +31,7 @@ const AlbumDetail = ({ album }) => {
       </CardSection>
 
       <CardSection>
-        <Button onPress={() => Linking.openURL(url)} />
+        <Button onPress={() => Linking.openURL(url)}>Buy Now</Button>
       </CardSection>
     </Card>
   );
